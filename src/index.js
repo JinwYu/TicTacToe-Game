55022d5d89a923ea15b57a3bbd8aeac1114efa13
Johan Yu <johanyu@Johans-MacBook-Pro.local>
2018-09-19 14:53:34 +0200

    Added functionality that keeps track of the previous turns in the game.

--- src/index.js
+++ src/index.js
@@ -35,58 +35,18 @@ import './index.css';
 
   class Board extends React.Component {
 
-    constructor(props) {
-      super(props);
-      this.state = {
-        squares: Array(9).fill(null),
-        xIsNext: true,
-      };
-      /*
-        Each time a player moves, xIsNext (a boolean) 
-        will be flipped to determine which player goes 
-        next and the game’s state will be saved. We’ll 
-        update the Board’s handleClick function to flip 
-        the value of xIsNext:
-      */
-    }
-
-    handleClick(i) {
-      const squares = this.state.squares.slice();
-
-      // Return early by ignoring a click if someone has won 
-      // the game or if a Square is already filled.
-      if(calculateWinner(squares) || squares[i]){
-        return;
-      } 
-
-      squares[i] = this.state.xIsNext ? 'X' : 'O';
-      this.setState({
-        squares: squares,
-        xIsNext: !this.state.xIsNext, // Flips the turn.
-      });
-    }
-
     renderSquare(i) {
       return (
         <Square 
-          value={this.state.squares[i]}
-          onClick={() => this.handleClick(i)}
+          value={this.props.squares[i]}
+          onClick={() => this.props.onClick(i)}
         />
       );
     }
   
     render() {
-      const winner = calculateWinner(this.state.squares);
-      let status;
-      if(winner) {
-        status = 'Winner: ' + winner;
-      } else{
-        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-      }
-
       return (
         <div>
-          <div className="status">{status}</div>
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -108,15 +68,114 @@ import './index.css';
   }
   
   class Game extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        history: [{
+          squares: Array(9).fill(null),
+        }],
+        stepNumber: 0,
+        xIsNext: true,
+        /*
+        Each time a player moves, xIsNext (a boolean) 
+        will be flipped to determine which player goes 
+        next and the game’s state will be saved. We’ll 
+        update the Board’s handleClick function to flip 
+        the value of xIsNext:
+      */
+      };
+    }
+
+    handleClick(i) {
+      // This ensures that if we “go back in time” and then
+      // make a new move from that point, we throw away all
+      // the “future” history that would now become incorrect.
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      
+      const current = history[history.length - 1];
+      const squares = current.squares.slice();
+
+      // Return early by ignoring a click if someone has won 
+      // the game or if a Square is already filled.
+      if(calculateWinner(squares) || squares[i]){
+        return;
+      } 
+
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        // concat() method doesn’t mutate the original array,
+        // unlike the array push() method.
+        history: history.concat([{
+          squares: squares,
+        }]),
+        // After we make a new move, we need to update 
+        // stepNumber by adding stepNumber: history.length
+        // as part of the this.setState argument. This ensures
+        // we don’t get stuck showing the same move after a
+        // new one has been made.
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext, // Flips the turn.
+      });
+    }
+
+    jumpTo(step){
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
+      });
+    }
+
     render() {
+      const history = this.state.history;
+      const current = history[this.state.stepNumber]; //Rendering the currently selected move according to stepNumber.
+      const winner = calculateWinner(current.squares);
+
+      /*
+        const numbers = [1, 2, 3];
+        const doubled = numbers.map(x => x * 2); // [2, 4, 6]
+        Using the map method, we can map our history of moves to
+        React elements representing buttons on the screen, and 
+        display a list of buttons to “jump” to past moves.
+      */
+      const moves = history.map((step, move) => {
+        const desc = move ? 
+        'Go to move #' + move : 
+        'Go to game start';
+
+        /*
+          For each move in the tic-tac-toes’s game’s history,
+          we create a list item <li> which contains a button
+          <button>. The button has a onClick handler which calls
+          a method called this.jumpTo().
+        */
+        return(
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>
+              {desc}
+            </button>
+          </li>
+        )
+      })
+  
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+  
       return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
+  
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
+            <div>{status}</div>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
